81607bc42e8fc3fa5e326b7e62502d9f61d0ad18
王教鼎 <onejiaoding@gmail.com>
2021-05-27 21:48:32 +0800

    add program exercise 2

---
+++ chapter_2/program_exercises/exercise_2_2.c
@@ -0,0 +1,15 @@
+/*
+ * @Author: 王教鼎
+ * @Date: 2021/5/20 9:37 下午
+ * @Description: 第2题
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    printf("name = jiaoding\n");
+    printf("address  = hangzhou");
+
+    return 0;
+}
+
