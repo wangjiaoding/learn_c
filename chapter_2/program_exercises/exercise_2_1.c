fdba57593aad0f6e16e6f686045a60db050a056c
王教鼎 <onejiaoding@gmail.com>
2021-05-27 21:48:21 +0800

    add program exercise 1

---
+++ chapter_2/program_exercises/exercise_2_1.c
@@ -0,0 +1,16 @@
+/*
+ * @Author: 王教鼎
+ * @Date: 2021/5/20 9:33 下午
+ * @Description: 第1题
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Gustav Mahler\n");
+    printf("Gustav\nMahler\n");
+    printf("Gustav ");
+    printf("Mahler");
+
+    return 0;
+}
