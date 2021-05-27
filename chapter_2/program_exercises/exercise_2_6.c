e4ffb8e5e42df6dc76b403836fc8e72c44a8686f
王教鼎 <onejiaoding@gmail.com>
2021-05-28 07:01:23 +0800

    add program exercise 6

---
+++ chapter_2/program_exercises/exercise_2_6.c
@@ -0,0 +1,19 @@
+/*
+ * @Author: 王教鼎
+ * @Date: 2021/5/21 9:25 下午
+ * @Description: 第6题
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    int toes = 10;
+    int toes_double = toes * 2;
+    int toes_square = toes * toes;
+
+    printf("toes = %d\n", toes);
+    printf("The double of toes = %d\n", toes_double);
+    printf("The square of toes = %d\n", toes_square);
+
+    return 0;
+}
