4b229fc9bb297b109fd5bd52678d8fa1e064c3f0
王教鼎 <onejiaoding@gmail.com>
2021-05-28 07:01:30 +0800

    add program exercise 7

---
+++ chapter_2/program_exercises/exercise_2_7.c
@@ -0,0 +1,28 @@
+/*
+ * @Author: 王教鼎
+ * @Date: 2021/5/21 9:43 下午
+ * @Description: 第7题
+ */
+#include <stdio.h>
+
+void smile(void);
+
+int main(void)
+{
+    smile();
+    smile();
+    smile();
+    printf("\n");
+    smile();
+    smile();
+    printf("\n");
+    smile();
+
+    return 0;
+}
+
+void smile(void)
+{
+    printf("Smile!");
+}
+
