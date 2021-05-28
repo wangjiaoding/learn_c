213d255cc0d03959414fbd2333528e604f80eea2
王教鼎 <onejiaoding@gmail.com>
2021-05-28 22:09:04 +0800

    add program list 2.1

---
+++ chapter_2/program_lists/first.c
@@ -0,0 +1,18 @@
+/*
+ * @Author: 王教鼎
+ * @Date: 2021/5/19 8:55 下午
+ * @Description: 程序清单2.1
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    int num;
+    num = 1;
+    printf("I am a simple ");
+    printf("computer.\n");
+    printf("My favorite number is %d because it is first.\n", num);
+    return 0;
+}
+
+
