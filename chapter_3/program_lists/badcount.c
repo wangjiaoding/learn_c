d974b6c05a508c14516ea1b78c35dc9eb85fa2fe
王教鼎 <onejiaoding@gmail.com>
2021-05-28 22:09:37 +0800

    add program list 3.9

---
+++ chapter_3/program_lists/badcount.c
@@ -0,0 +1,20 @@
+/*
+ * @Author: 王教鼎
+ * @Date: 2021/5/23 9:19 下午
+ * @Description: 程序清单3.9
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    int n = 4;
+    int m = 5;
+    float f = 7.0f;
+    float g = 8.0f;
+
+    printf("%d\n", n, m);
+    printf("%d %d %d\n", n);
+    printf("%d %d\n", f, g);
+
+    return 0;
+}
