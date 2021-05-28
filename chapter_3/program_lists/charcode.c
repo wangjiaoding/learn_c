a43677b592478337135586d943c63416d406aa33
王教鼎 <onejiaoding@gmail.com>
2021-05-28 22:09:51 +0800

    add program list 3.5

---
+++ chapter_3/program_lists/charcode.c
@@ -0,0 +1,16 @@
+/*
+ * @Author: 王教鼎
+ * @Date: 2021/5/23 2:39 下午
+ * @Description: 程序清单3.5
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    char ch;
+    printf("Please enter a character.\n");
+    scanf("%c", &ch);
+    printf("The code for %c is %d.\n", ch, ch);
+
+    return 0;
+}
