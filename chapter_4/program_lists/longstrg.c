54b741fa0ae56934688fcc234e10eb4d9aee79c1
王教鼎 <onejiaoding@gmail.com>
2021-05-29 08:50:54 +0800

    add program list 4.14

---
+++ chapter_4/program_lists/longstrg.c
@@ -0,0 +1,18 @@
+/*
+ * @Author: 王教鼎
+ * @Date: 2021/5/27 11:33 下午
+ * @Description: 程序清单4.14
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Here's one way to print a ");
+    printf("long string.\n");
+    printf("Here's another way to print a \
+long string.\n");
+    printf("Here's the newest way to ptrint a" \
+           "long string.\n");
+    return 0;
+}
+
