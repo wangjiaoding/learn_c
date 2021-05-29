c7550b6189fe7cbdef0ee1ff470176e7dfe21e95
王教鼎 <onejiaoding@gmail.com>
2021-05-29 08:51:54 +0800

    add program exercise 2

---
+++ chapter_3/program_exercises/exercise_3_2.c
@@ -0,0 +1,15 @@
+/*
+ * @Author: 王教鼎
+ * @Date: 2021/5/24 10:19 下午
+ * @Description: 第2题
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Please enter a ASCII value: ");
+    int value;
+    scanf("%d", &value);
+    printf("You enter is %c's ASCII.\n", value);
+    return 0;
+}
