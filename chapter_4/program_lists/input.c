ea659e2afddb731cf99aadd81009ebb48ec50f3d
王教鼎 <onejiaoding@gmail.com>
2021-05-29 08:50:46 +0800

    add program list 4.5

---
+++ chapter_4/program_lists/input.c
@@ -0,0 +1,19 @@
+/*
+ * @Author: 王教鼎
+ * @Date: 2021/5/28 10:24 下午
+ * @Description: 程序清单4.5
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    int age;
+    float assets;
+    char pet[30];
+
+    printf("Enter your age, assets, and favorite pet.\n");
+    scanf("%d %f", &age, &assets);
+    scanf("%s", pet);
+    printf("%d $%.2f %s\n", age, assets, pet);
+    return 0;
+}
