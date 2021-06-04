b232ca9d2b265efdba6464259051f247ad41f2d7
王教鼎 <onejiaoding@gmail.com>
2021-06-04 23:46:38 +0800

    add program list 6.20

--- chapter_6/program_lists/powwer.c
+++ chapter_6/program_lists/powwer.c
@@ -11,16 +11,13 @@ int main(void)
 {
     double x, xpow;
     int exp;
-    printf("Enter a number and the positive integer "
-           "power");
-    printf(" to which\nthe number will be raised. Enter "
-           "q");
+    printf("Enter a number and the positive integer power");
+    printf(" to which\nthe number will be raised. Enter q");
     printf(" to quit.\n");
     while (scanf("%lf%d", &x, &exp) == 2)
     {
         xpow = power(x, exp);
-        printf("%.3g to the power %d is %.5g\n", x, exp,
-               xpow);
+        printf("%.3g to the power %d is %.5g\n", x, exp,xpow);
         printf("Enter next pair of number or q to quit.\n");
     }
     printf("Hope you enjoy this power trip -- bye!\n");
