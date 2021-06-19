79b948441af26c1c25bc4356c063a55b4c19dfdc
王教鼎 <onejiaoding@gmail.com>
2021-06-20 00:59:05 +0800

    add program list 11.2

---
+++ chapter_11/program_lists/strptr.c
@@ -0,0 +1,12 @@
+/*
+ * @Author: 王教鼎
+ * @Date: 2021/6/19 9:01 下午
+ * @Description: 程序清单11.2
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%s, %p, %c\n", "We", "are", *"space farers");
+    return 0;
+}
