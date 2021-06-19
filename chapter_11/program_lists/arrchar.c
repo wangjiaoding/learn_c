82233ae91c7098ef43d8316cee66bf0f875d3fe3
王教鼎 <onejiaoding@gmail.com>
2021-06-20 00:58:40 +0800

    add program list 11.4

---
+++ chapter_11/program_lists/arrchar.c
@@ -0,0 +1,36 @@
+/*
+ * @Author: 王教鼎
+ * @Date: 2021/6/19 9:17 下午
+ * @Description: 程序清单11.4
+ */
+#include <stdio.h>
+
+#define SLEN 40
+#define LIM 5
+
+int main(void)
+{
+    const char * mytalents[LIM] = {
+            "Adding numbers swiftly",
+            "Multiplying accurately", "Stashing data",
+            "Following instructions to the letter",
+            "Understanding the C language"
+    };
+    char yourtalents[LIM][SLEN] = {
+            "Walking in a straight line",
+            "Sleeping", "Watching television",
+            "Mailing letters", "Reading email"
+    };
+    int i;
+    puts("Let's compare talents.");
+    printf("%-36s   %-25s\n", "My Talents", "Your Talents");
+    for (i = 0; i < LIM; i++)
+        printf("%-36s   %-25s\n", mytalents[i], yourtalents[i]);
+    printf("\nsizeof mytalents: %zd, sizeof yourtalents: %zd\n",
+           sizeof(mytalents), sizeof(yourtalents));
+
+    const char * str = "wangjiaoding";
+    printf("str = %zd", sizeof(char *));
+    printf("char = %zd", sizeof(char));
+    return 0;
+}
