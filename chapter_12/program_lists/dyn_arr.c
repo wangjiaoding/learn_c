58174aff1d82a169e3aff7de9844c98dd6e67375
王教鼎 <onejiaoding@gmail.com>
2021-07-03 23:00:47 +0800

    add program list 12.14

---
+++ chapter_12/program_lists/dyn_arr.c
@@ -0,0 +1,42 @@
+/*
+ * @Author: 王教鼎
+ * @Date: 2021/7/3 7:37 下午
+ * @Description: 程序清单12.14
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double * ptd;
+    int max;
+    int number;
+    int i = 0;
+    puts("What is the maximum number of type double entries?");
+    if (scanf("%d", &max) != 1)
+    {
+        puts("Number not correctly entered bye.");
+        exit(EXIT_FAILURE);
+    }
+    ptd = (double *)malloc(max * sizeof(double ));
+    if (ptd == NULL)
+    {
+        puts("Memory allocation failed. Goodbye.");
+        exit(EXIT_FAILURE);
+    }
+    puts("Enter the values (q to quit):");
+    while (i < max  && scanf("%lf", &ptd[i]) == 1)
+        ++i;
+    printf("Here are your %d entries:\n", number = i);
+    for (i = 0; i < number; i++)
+    {
+        printf("%7.2f ", ptd[i]);
+        if (i % 7 == 6)
+            putchar('\n');
+    }
+    if (i % 7 != 0)
+        putchar('\n');
+    puts("Done.");
+    free(ptd);
+    return 0;
+}
