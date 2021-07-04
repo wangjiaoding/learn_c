0412c216ccd715398a3856c816e350be02a716b7
王教鼎 <onejiaoding@gmail.com>
2021-07-05 00:15:49 +0800

    add program list 14.4

---
+++ chapter_14/program_lists/friends.c
@@ -0,0 +1,47 @@
+/*
+ * @Author: 王教鼎
+ * @Date: 2021/7/5 12:04 上午
+ * @Description: 程序清单14.4
+ */
+#include <stdio.h>
+#define LEN 20
+
+struct names {
+    char first[LEN];
+    char last[LEN];
+};
+
+struct guy {
+    struct names handle;
+    char favfood[LEN];
+    char job[LEN];
+    float income;
+};
+
+int main(void)
+{
+    struct guy fellow[2] = {
+            {
+                    {"Ewen", "Villard"},
+                    "grilled salmon",
+                    "personality coach",
+                    68112.00
+            },
+            {
+                    { "Rodney", "Swillbelly" },
+                    "tripe",
+                    "tabloid editor",
+                    432400.00
+            }
+    };
+    struct guy * him;
+    printf("address #1: %p #2: %p\n", &fellow[0], &fellow[1]);
+    him = &fellow[0];
+    printf("pointer #1: %p #2: %p\n", him, him + 1);
+    printf("him->income is $%.2f: (*him).income is $%.2f\n", him->income,
+           (*him).income);
+    him++;
+    printf("him->favfood is %s: him->handle.last is %s\n", him->favfood,
+           him->handle.last);
+    return 0;
+}
